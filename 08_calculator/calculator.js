537739e0c863df901ba6ba54321d8e49ba9a5bf0
Arlots <ethan.jennings092@gmail.com>
2025-01-29 14:28:41 +0900

    completed calculator javascript exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const initialValue = 0;
+  if (arr[0] === undefined) {
+    return initialValue;
+  }
+  const sumAll = arr.reduce((total, currentValue) => total + currentValue, initialValue
+  
+)
+return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+const initialValue = 1;
+const productAll = arr.reduce((total, currentValue) => total * currentValue, initialValue); 
+return productAll;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  
+  if (a === 0 || a === 1) {
+    return 1;
+  }
+  
+  for (i = a-1 ; i >= 1; i--) {
+    a *= i;
+  }
+
+  return a;
 };
 
 // Do not edit below this line
