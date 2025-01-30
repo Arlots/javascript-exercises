011001997e3aa735286c88e3f55b20c3b134eb05
Arlots <ethan.jennings092@gmail.com>
2025-01-30 16:41:46 +0900

    completed palindrome tests after many failures

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,33 @@
-const palindromes = function () {
+const palindromes = function (palindrome) {
+
+    const palindromeArray = Array.from(palindrome);
+    
+    const filteredPalindrome = palindromeArray.filter(function (letter) {
+        if (letter == '!' || letter == ',' || letter == '.' || letter == ' ') {
+            return false;
+        } else {
+            return true;
+        }
+    });
+    const toBeReversed = filteredPalindrome.slice();
+    
+    const reversedArray = toBeReversed.reverse();
+
+    let finalFilter = filteredPalindrome.toString().toLowerCase();
+    let finalReverse = reversedArray.toString().toLowerCase();
+    
+    console.log(filteredPalindrome);
+    console.log(finalFilter);
+    console.log(reversedArray);
+    console.log(finalReverse);
+    
+        
+    if (finalFilter  === finalReverse) {
+        return true;
+    } else if (finalFilter  != finalReverse) {
+        return false;
+    }
+    
 
 };
 
